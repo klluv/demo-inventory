9f656e6afb15c5c648f4e5425e1cf0bc8c9de3cc
Andhika Herdha Jati Samudra <101403750+klluv@users.noreply.github.com>
2023-07-20 11:08:10 +0700

    Update Kaka.vue

--- src/components/Kaka.vue
+++ src/components/Kaka.vue
@@ -6,23 +6,47 @@
             <h3 v-show="kaka.show">{{ kaka.speciality }}</h3>
             </li>
         </ul>
+        <button @click="deleteKaka">Delete Kaka</button>
     </div>
 </template>
   
   <script>
   
   export default {
+    props: {
+        kakas: {
+            type: Array,
+            required: true
+        }
+    },
     data () {
         return {
-            kakas: [
-                {name: 'Sukri', speciality: 'Vue Components', show: false},
-                {name: 'Seno', speciality: 'HTML Wizardry', show: false},
-                {name: 'Rope', speciality: 'Click Events', show: false},
-                {name: 'Bonar', speciality: 'Conditionals', show: false},
-                {name: 'Luci', speciality: 'Webpack', show: false},
-                {name: 'Jess', speciality: 'Data Diggin', show: false}
-            ]
+            
         }
+    },
+    methods: {
+        deleteKaka: function() {
+            this.kakas.pop()
+        }
+    },
+    //lifecyle hooks
+    beforeCreate(){
+        alert("beforeCreate");
+    },
+    created(){
+        alert("Created");
+    },
+    beforeMount() {
+        alert("beforeMount");
+    },
+    mounted(){
+        alert("mounted");
+    },
+    beforeUpdate(){
+        alert("beforeUpdtae");
+    },
+    updated(){
+        alert("updated");
     }
   }
   </script>
