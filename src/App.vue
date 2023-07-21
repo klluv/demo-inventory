5cd21b02fa238d73a586f208a6bf5b980ce43635
Andhika Herdha Jati Samudra <101403750+klluv@users.noreply.github.com>
2023-07-21 10:11:10 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
-  <div>
-    <app-header></app-header>
-    <app-kaka></app-kaka>
-    <app-footer></app-footer>
-  </div>
+    <add-blog></add-blog>
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';  
-import Kaka from './components/Kaka.vue';   
+import addBlog from './components/addBlog.vue';
 
 export default {
-  components: {
-    'app-header':Header,
-    'app-footer':Footer,
-    'app-kaka':Kaka
-  },
-  data () {
-      return {
-       
-      }
-  }
+    components: {
+        'add-blog': addBlog
+    },
+
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
 }
 </script>
 
 <style>
-
-
-
+body{
+    margin: 0;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
 </style>
